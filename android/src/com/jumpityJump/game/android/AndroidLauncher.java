467e5fb83d8986cac20e29882fb877f8ce9f9aab
Jose <josecardos@gmail.com>
2014-06-04 16:36:24 +0100

    commit de tudo

---
+++ android/src/com/jumpityJump/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jumpityJump.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jumpityJump.game.JumpityJump;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JumpityJump(), config);
+	}
+}
